b25cd6583cc3748dde0b2a83931e96f3da8a1a05
OgerMW <45423293+OgerMW@users.noreply.github.com>
2025-01-23 23:03:22 +0300

    Update setup_vnc.sh

--- setup_vnc.sh
+++ setup_vnc.sh
@@ -16,7 +16,7 @@ center_text() {
     local text="\$1"
     local cols=$(tput cols)
     local length=${#text}
-    local padding=$(( ($cols - $length) / 2 ))
+    local padding=$(( (cols - length) / 2 ))
     printf "%${padding}s\n" "$text"
 }
 
@@ -28,7 +28,7 @@ green_text() {
     tput sgr0 # Сбрасываем настройки формата
 }
 
-# Функция для анимации
+# Функция анимации
 show_loading() {
     local pid="\$1"
     local delay=0.75
@@ -41,15 +41,13 @@ show_loading() {
     printf "\r"  # Очистить строку после завершения
 }
 
-# Отключаем уведомления через конфигурацию APT
+# Начало установки
 green_text "Начинаем установку. Все операции займут около 5-7 минут."
-log_message "Отключаем уведомления через конфигурацию APT"
-{
-    echo "Unattended-Upgrade::Automatic-Reboot \"false\";"
-    echo "Unattended-Upgrade::Automatic-Reboot-WithUsers \"false\";"
-    echo "APT::Periodic::Unattended-Upgrade \"0\";"
-    echo "APT::Periodic::Unattended-Upgrade \"0\";"
-} | sudo tee -a /etc/apt/apt.conf.d/50unattended-upgrades &>> "$LOG_FILE" &
+log_message "Начинаем установку. Все операции займут около 5-7 минут."
+echo "Unattended-Upgrade::Automatic-Reboot \"false\";" | sudo tee -a /etc/apt/apt.conf.d/50unattended-upgrades
+echo "Unattended-Upgrade::Automatic-Reboot-WithUsers \"false\";" | sudo tee -a /etc/apt/apt.conf.d/50unattended-upgrades
+echo "APT::Periodic::Unattended-Upgrade \"0\";" | sudo tee -a /etc/apt/apt.conf.d/99needrestart
+echo "APT::Periodic::Unattended-Upgrade \"0\";" | sudo tee -a /etc/apt/apt.conf.d/10periodic &>> "$LOG_FILE" &
 show_loading $!
 
 # Установка необходимых пакетов
@@ -62,17 +60,17 @@ log_message "Установка необходимых пакетов"
 show_loading $!
 
 # Создаем пользователя vnc
-green_text "Создаем пользователя vnc"
+green_text "Создаем пользователя vnc. Устанавливаем User - vnc / Устанавливаем Password - 172029"
 log_message "Создаем пользователя vnc"
 {
-    sudo useradd -m -s /bin/bash ${VNC_USER}
-    sudo usermod -aG sudo ${VNC_USER}
+    sudo useradd -m -s /bin/bash "${VNC_USER}"
+    sudo usermod -aG sudo "${VNC_USER}"
     echo "${VNC_USER}:172029" | sudo chpasswd
 } &>> "$LOG_FILE" &
 show_loading $!
 
 # Настраиваем VNC
-green_text "Настраиваем VNC"
+green_text "Настраиваем VNC. Устанавливаем Password - 172029"
 log_message "Настраиваем VNC"
 LANG=en_US.UTF-8 expect -c "
     spawn sudo -u ${VNC_USER} vncpasswd;
@@ -89,21 +87,37 @@ show_loading $!
 # Создание файла xstartup
 green_text "Создание файла xstartup"
 log_message "Создание файла xstartup"
-sudo -u ${VNC_USER} bash -c 'cat << EOF > ~/.vnc/xstartup
+sudo -u "${VNC_USER}" bash -c 'cat << EOF > ~/.vnc/xstartup
 #!/bin/bash
 xrdb \$HOME/.Xresources
 autocutsel -fork
 startxfce4 &
 EOF' &>> "$LOG_FILE" &
 show_loading $!
-sudo -u ${VNC_USER} chmod +x ~/.vnc/xstartup &>> "$LOG_FILE" &
+sudo -u "${VNC_USER}" chmod +x ~/.vnc/xstartup &>> "$LOG_FILE" &
 show_loading $!
 
 # Создание systemd сервиса для VNC
 green_text "Создание systemd сервиса для VNC"
-log_message "Создание systemd сервиса для VNC"
-cat << EOF | sudo tee /etc/systemd/system/vncserver@.service &>> "$LOG_FILE" &
-show_loading $!
+cat << EOF | sudo tee /etc/systemd/system/vncserver@.service
+[Unit]
+Description=Start VNC server at startup
+After=syslog.target network.target
+
+[Service]
+Type=forking
+User =${VNC_USER}
+Group=${VNC_USER}
+WorkingDirectory=/home/${VNC_USER}
+
+PIDFile=/home/${VNC_USER}/.vnc/%H:%i.pid
+ExecStartPre=-/usr/bin/vncserver -kill :%i > /dev/null 2>&1
+ExecStart=/usr/bin/vncserver -depth 24 -geometry 1920x1080 :%i
+ExecStop=/usr/bin/vncserver -kill :%i
+
+[Install]
+WantedBy=multi-user.target
+EOF
 
 # Включение и запуск VNC сервиса
 green_text "Включение и запуск VNC сервиса"
