aaeca5cb071d7d1fff74ec8807823a7f5458b132
OgerMW <45423293+OgerMW@users.noreply.github.com>
2025-01-23 21:46:05 +0300

    Update setup_vnc.sh

--- setup_vnc.sh
+++ setup_vnc.sh
@@ -17,25 +17,27 @@ center_text() {
 green_text() {
     tput bold # Включаем жирный шрифт
     tput setaf 2 # Устанавливаем зеленый цвет
-    printf "\e[30;1;4m%s\e[0m\n" "$1" # Увеличенный размер текста
+    center_text "$1"
+    tput sgr0 # Сбрасываем настройки формата
 }
 
 # Отключаем уведомления через конфигурацию APT
 green_text "Отключаем уведомления через конфигурацию APT"
-echo "Unattended-Upgrade::Automatic-Reboot \"false\";" | sudo tee -a /etc/apt/apt.conf.d/50unattended-upgrades
-echo "Unattended-Upgrade::Automatic-Reboot-WithUsers \"false\";" | sudo tee -a /etc/apt/apt.conf.d/50unattended-upgrades
-echo "APT::Periodic::Unattended-Upgrade \"0\";" | sudo tee -a /etc/apt/apt.conf.d/10periodic
+echo "Unattended-Upgrade::Automatic-Reboot \"false\";" | sudo tee -a /etc/apt/apt.conf.d/50unattended-upgrades &>/dev/null
+echo "Unattended-Upgrade::Automatic-Reboot-WithUsers \"false\";" | sudo tee -a /etc/apt/apt.conf.d/50unattended-upgrades &>/dev/null
+echo "APT::Periodic::Unattended-Upgrade \"0\";" | sudo tee -a /etc/apt/apt.conf.d/10periodic &>/dev/null
+echo "APT::Periodic::Unattended-Upgrade \"0\";" | sudo tee -a /etc/apt/apt.conf.d/99needrestart &>/dev/null
 
 # Установка необходимых пакетов
 green_text "Установка необходимых пакетов"
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install -y xfce4 xfce4-goodies tightvncserver autocutsel expect
+sudo apt-get update -qq && sudo apt-get upgrade -y -q &>/dev/null
+sudo apt-get install -y -q xfce4 xfce4-goodies tightvncserver autocutsel expect &>/dev/null
 
 # Создаем пользователя vnc
 green_text "Создаем пользователя vnc"
-sudo useradd -m -s /bin/bash ${VNC_USER}
-sudo usermod -aG sudo ${VNC_USER}
-echo "${VNC_USER}:172029" | sudo chpasswd ${VNC_USER}
+sudo useradd -m -s /bin/bash ${VNC_USER} &>/dev/null
+sudo usermod -aG sudo ${VNC_USER} &>/dev/null
+echo "${VNC_USER}:172029" | sudo chpasswd ${VNC_USER} &>/dev/null
 
 # Настраиваем VNC
 green_text "Настраиваем VNC"
@@ -48,7 +50,7 @@ LANG=en_US.UTF-8 expect -c "
     expect \"Would you like to enter a view-only password (y/n)?\";
     send \"n\r\";
     interact
-"
+" &>/dev/null
 
 # Создание файла xstartup
 green_text "Создание файла xstartup"
@@ -57,13 +59,12 @@ sudo -u ${VNC_USER} bash -c 'cat << EOF > ~/.vnc/xstartup
 xrdb \$HOME/.Xresources
 autocutsel -fork
 startxfce4 &
-EOF'
-
-sudo -u ${VNC_USER} chmod +x ~/.vnc/xstartup
+EOF' &>/dev/null
+sudo -u ${VNC_USER} chmod +x ~/.vnc/xstartup &>/dev/null
 
 # Создание systemd сервиса для VNC
 green_text "Создание systemd сервиса для VNC"
-cat << EOF | sudo tee /etc/systemd/system/vncserver@.service
+cat << EOF | sudo tee /etc/systemd/system/vncserver@.service &>/dev/null
 [Unit]
 Description=Start VNC server at startup
 After=syslog.target network.target
@@ -85,13 +86,13 @@ EOF
 
 # Включение и запуск VNC сервиса
 green_text "Включение и запуск VNC сервиса"
-sudo systemctl daemon-reload
-sudo systemctl enable vncserver@${DISPLAY_NUMBER}
-sudo systemctl start vncserver@${DISPLAY_NUMBER}
+sudo systemctl daemon-reload &>/dev/null
+sudo systemctl enable vncserver@${DISPLAY_NUMBER} &>/dev/null
+sudo systemctl start vncserver@${DISPLAY_NUMBER} &>/dev/null
 
 # Проверяем статус службы
 green_text "Проверка статуса службы VNC"
-sudo systemctl status vncserver@${DISPLAY_NUMBER}
+sudo systemctl status vncserver@${DISPLAY_NUMBER} &>/dev/null
 
 # Предложение о перезагрузке
 green_text "Настройка VNC сервера завершена. Вы можете подключиться к серверу. Для завершения настроек рекомендуется перезагрузить систему. Выполнить перезагрузку сейчас? (Y/N)"
