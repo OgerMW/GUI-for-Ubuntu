9cbdbff0e0824217b9fcec1873c19f327fa98c7a
OgerMW <45423293+OgerMW@users.noreply.github.com>
2025-01-25 17:54:23 +0300

    Update setup_vnc.sh

--- setup_vnc.sh
+++ setup_vnc.sh
@@ -29,7 +29,7 @@ echo 'APT::Periodic::Unattended-Upgrade "0";' | sudo tee -a /etc/apt/apt.conf.d/
 echo 'APT::Periodic::Unattended-Upgrade "0";' | sudo tee -a /etc/apt/apt.conf.d/10periodic > /dev/null & 
 
 # Установка необходимых пакетов и обновление системы
-print_green "Установка обновлений системы..."
+print_green "Устанавливаем обновления системы..."
 apt install sudo -y > /dev/null 2>&1 &
 spinner $!
 sudo apt update > /dev/null 2>&1 &
@@ -37,7 +37,7 @@ spinner $!
 sudo apt upgrade -y > /dev/null 2>&1 &
 spinner $!
 
-print_green "Установка дополнительных компонентов..."
+print_green "Устанавливаем дополнительные компоненты..."
 sudo apt install -y xfce4 xfce4-goodies tightvncserver autocutsel expect > /dev/null 2>&1 &
 spinner $!
 
@@ -64,19 +64,19 @@ sudo update-alternatives --config x-terminal-emulator <<< "2" > /dev/null 2>&1 &
 spinner $!
 
 # Создание пользователя vnc и настройка прав
-print_green "Создание пользователя vnc..."
+print_green "Создаём пользователя vnc..."
 useradd -m -s /bin/bash vnc > /dev/null 2>&1 &
 spinner $!
 usermod -aG sudo vnc > /dev/null 2>&1 &
 spinner $!
 
 # Установка пароля для пользователя vnc
-print_green "Установите пароль для пользователя vnc:"
+print_green "Устанавливаем пароль для пользователя vnc:"
 echo -e "172029\n172029" | sudo passwd vnc > /dev/null 2>&1 &
 spinner $!
 
 # Настройка VNC
-print_green "Настройка VNC-сервера..."
+print_green "Настраиваем VNC-сервер..."
 sudo -u vnc expect <<EOS > /dev/null 2>&1
 spawn vncpasswd
 expect "Password:"
@@ -99,7 +99,7 @@ EOL
 sudo chmod 755 /home/vnc/.vnc/xstartup
 
 # Создание и настройка systemd-юнита для VNC
-print_green "Настройка systemd-юнита для VNC..."
+print_green "Настаиваем systemd-юнит для VNC..."
 sudo bash -c 'cat <<EOL > /etc/systemd/system/vncserver@.service
 [Unit]
 Description=Start VNC server at startup
@@ -121,7 +121,7 @@ WantedBy=multi-user.target
 EOL'
 
 # Включение и запуск службы VNC
-print_green "Включение и запуск службы VNC..."
+print_green "Включаем и запускаем службы VNC..."
 sudo systemctl enable vncserver@1 > /dev/null 2>&1 &
 spinner $!
 sudo systemctl start vncserver@1 > /dev/null 2>&1 &
