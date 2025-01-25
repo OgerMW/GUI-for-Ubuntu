df437a47754e85f8d866ed66c2e11917d40ac461
OgerMW <45423293+OgerMW@users.noreply.github.com>
2025-01-25 17:58:18 +0300

    Update setup_vnc.sh

--- setup_vnc.sh
+++ setup_vnc.sh
@@ -71,7 +71,7 @@ usermod -aG sudo vnc > /dev/null 2>&1 &
 spinner $!
 
 # Установка пароля для пользователя vnc
-print_green "Устанавливаем пароль для пользователя vnc:"
+print_green "Устанавливаем пароль для пользователя vnc..."
 echo -e "172029\n172029" | sudo passwd vnc > /dev/null 2>&1 &
 spinner $!
 
