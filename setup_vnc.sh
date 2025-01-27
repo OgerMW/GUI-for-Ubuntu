fbe7c810f1af98386e1714e9078ffd8fc41a3fab
Alexander <45423293+OgerMW@users.noreply.github.com>
2025-01-27 14:24:43 +0300

    Update setup_vnc.sh

--- setup_vnc.sh
+++ setup_vnc.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Функция для вывода текста зеленым цветом
 print_green() {
     echo -e "\e[32m$1\e[0m"
@@ -28,15 +29,15 @@ spinner() {
     printf "\r      \r"
 }
 
-# Перенаправление вывода в файл логов
-exec > >(tee -i install.log)
-exec 2>&1
-
 # Проверка прав администратора
 if [ "$EUID" -ne 0 ]; then
     print_green "Этот скрипт нужно запускать с правами администратора (sudo)."
     exit 1
 fi
+
+# Перенаправляем stdout и stderr в файл логов, но оставляем stdin без изменений
+exec 3>&1 4>&2
+exec > >(tee -i install.log) 2>&1
 
 # Меню выбора версии скрипта
 print_green "Выберите версию скрипта:"
@@ -44,14 +45,14 @@ print_green "1) Полностью автоматическая установк
 print_green "2) Ручная установка"
 
 while true; do
-    read -p "Введите номер варианта: " choice
+    read -u 0 -p "Введите номер варианта: " choice
     case $choice in
         1)
             VNC_PASSWORD="172029"
             break
             ;;
         2)
-            read -s -p "Введите пароль для пользователя vnc: " VNC_PASSWORD
+            read -s -u 0 -p "Введите пароль для пользователя vnc: " VNC_PASSWORD
             echo  # Для новой строки после ввода пароля
             break
             ;;
@@ -60,3 +61,6 @@ while true; do
             ;;
     esac
 done
+
+# Восстановление стандартных потоков
+exec 1>&3 2>&4
